8ba9799c72eb274b8d960126c3cef8f3a08586b9
jbeardwo <jfwbeardwood@gmail.com>
2025-06-29 22:18:31 -0700

    Homepage but sleeker this time.

--- src/HomeView.tsx
+++ src/HomeView.tsx
@@ -1,91 +1,44 @@
 import React, { useState } from 'react';
-import { motion, useAnimationControls } from 'framer-motion';
+import { motion } from 'framer-motion';
 import './HomeView.css'; 
 
 interface HomeViewProps {
   onSelectView: (view: 'creative' | 'technical') => void;
 }
 
-const CREATIVE_BG_IMAGE = '/images/creative-bg.jpg';
-const TECHNICAL_BG_IMAGE = '/public/images/technical-bg.jpg';
-const DEFAULT_BG_IMAGE = '/public/images/technical-bg1.jpg';
-
 
 function HomeView({ onSelectView }: HomeViewProps) {
 
   const [isCreativeClicked, setIsCreativeClicked] = useState(false);
   const [isTechnicalClicked, setIsTechnicalClicked] = useState(false);
-  const [bgImagePath, setBgImagePath] = useState(DEFAULT_BG_IMAGE);
 
-  const bgAnimationControls = useAnimationControls();
 
   const handleCreativeClick = async () => {
     setIsCreativeClicked(true);
     setIsTechnicalClicked(false);
-    setBgImagePath(CREATIVE_BG_IMAGE);
-    
-    await bgAnimationControls.set({ x: "-50vw", backgroundPosition: "right" });
-    await bgAnimationControls.start({
-        x: "0vw",
-        transition: { type: "spring", duration: .5, bounce: 0 }
-    });
 
     setTimeout(() => {
-     onSelectView('creative');
-    }, 150);
+    onSelectView('creative');
+    }, 600);
   };
 
   const handleTechnicalClick = async () => {
       setIsTechnicalClicked(true);
       setIsCreativeClicked(false);
-      setBgImagePath(TECHNICAL_BG_IMAGE);
-
-      await bgAnimationControls.set({ x: "50vw" , backgroundPosition: "left"});
-      await bgAnimationControls.start({
-        x:"0vw",
-        transition: { type: "spring", duration: .5, bounce: 0 }
-      })
 
     setTimeout(() => {
       onSelectView('technical');
-    }, 150);
+    }, 600);
   };
 
   return (
     <div className="home-view full-screen-view">
 
       <motion.div 
-        className="full-background-layer"
-        initial={{ x: "-50vw", opacity: 1 }}
-        animate={bgAnimationControls}
-        transition={{ type: "spring", duration: .5, bounce: 0 }}
-        style={{ backgroundImage: `url(${bgImagePath})`  }}
-      >
-      </motion.div>
-
-      <div className="page-title">
-        My Awesome Portfolio
-      </div>
-
-      <motion.div 
-        className="image-half left-half"
+        className="full-background-layer left-half"
         onClick={handleCreativeClick}
-        initial={{ x: 0, opacity: 1 }}
-        //Moves image across screen, width adjustment is to prevent sub-pixel overlap issues
-        animate={isCreativeClicked ? { x: "50vw", opacity:1, zIndex: 3, width: "50.1vw" } : { x:0, opacity:1, zIndex: 2 }}
-        transition={{ type: "spring", duration: .5, bounce: 0 }}
-      >
-        <div className="half-content">
-          <h2>Creative Work</h2>
-          <p>Explore my artistic portfolio.</p>
-        </div>
-      </motion.div>
-
-      <motion.div 
-        className="image-half right-half"
-        onClick={handleTechnicalClick}
-        initial={{ x: 0, opacity: 1 }}
-        animate={isTechnicalClicked ? { x: "-50vw", opacity:1, zIndex: 3, width: "50.1vw" } : { x:0, opacity:1, zIndex: 2 }}
+        initial={{ x: "-50vw", opacity: 1 }}
+        animate={isCreativeClicked ? { x: "0vw", opacity:1, zIndex:2 } : { x: "-50vw", opacity: 1 }}
         transition={{ type: "spring", duration: .5, bounce: 0 }}
       >
         <div className="half-content">
@@ -93,6 +46,25 @@ function HomeView({ onSelectView }: HomeViewProps) {
           <p>Explore my technical portfolio.</p>
         </div>
       </motion.div>
+
+      <motion.div 
+        className="full-background-layer right-half"
+        onClick={handleTechnicalClick}
+        initial={{ x: "50vw", opacity: 1 }}
+        animate={isTechnicalClicked ? { x: "0vw", opacity:1, zIndex:2 } : { x: "50vw", opacity: 1 }}
+        transition={{ type: "spring", duration: .5, bounce: 0 }} 
+      >
+        <div className="half-content">
+          <h2>Technical Work</h2>
+          <p>Explore my technical portfolio.</p>
+        </div>
+
+      </motion.div>
+
+      <div className="page-title">
+        My Awesome Portfolio
+      </div>
+
     </div>
   );
 }
